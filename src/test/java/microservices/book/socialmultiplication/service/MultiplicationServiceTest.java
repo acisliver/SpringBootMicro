5116b9daad6bba6effceb47f3086a649d6e4e2e9
acisliver <7273322@naver.com>
2022-03-26 14:38:18 +0900

    테스트를 먼저 작성하고 목 객체로 테스트 후 구현체 작성

--- src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceTest.java
+++ src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceTest.java
@@ -2,7 +2,7 @@ package microservices.book.socialmultiplication.service;
 
 import microservices.book.socialmultiplication.domain.Multiplication;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
