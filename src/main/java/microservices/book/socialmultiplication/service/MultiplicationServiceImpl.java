b001af83288deb49eef50140ad07cabe1a9e94d7
acisliver <7273322@naver.com>
2022-04-04 05:28:54 +0900

    비즈니스 로직 레이어 요구사항 - 제출한 답안의 정답 여부 확인 (로직 완성)

--- src/main/java/microservices/book/socialmultiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/microservices/book/socialmultiplication/service/MultiplicationServiceImpl.java
@@ -24,7 +24,9 @@ public class MultiplicationServiceImpl implements MultiplicationService{
 
     @Override
     public boolean checkAttempt(MultiplicationResultAttempt resultAttempt) {
-        return false;
+        return resultAttempt.getResultAttempt() ==
+                resultAttempt.getMultiplication().getFactorA() *
+                        resultAttempt.getMultiplication().getFactorB();
     }
 
 
