6c85c4ef4e2d5d60d0a9a88a7db2a1149298eb74
acisliver <7273322@naver.com>
2022-04-04 05:27:25 +0900

    비즈니스 로직 레이어 요구사항 - 제출한 답안의 정답 여부 확인 (테스트 코드 작성)

--- src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceImplTest.java
@@ -1,6 +1,8 @@
 package microservices.book.socialmultiplication.service;
 
 import microservices.book.socialmultiplication.domain.Multiplication;
+import microservices.book.socialmultiplication.domain.MultiplicationResultAttempt;
+import microservices.book.socialmultiplication.domain.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -24,16 +26,46 @@ public class MultiplicationServiceImplTest {
     }
 
     @Test
-    public void createRandomMultiplicationTest() {
-        // given (목 객체가 처음에 50, 다음에 30을 반환하도록 설정)
-        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+    public void checkCorrectAttemptTest() {
+        // given
+        Multiplication multiplication = new Multiplication(50, 60);
+        User user = new User("Daeeun");
+        MultiplicationResultAttempt attempt =
+                new MultiplicationResultAttempt(user, multiplication, 3000);
 
         // when
-        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+        boolean result = multiplicationServiceImpl.checkAttempt(attempt)
 
         // then
-        assertThat(multiplication.getFactorA()).isEqualTo(50);
-        assertThat(multiplication.getFactorB()).isEqualTo(30);
-        assertThat(multiplication.getResult()).isEqualTo(1500);
+        assertThat(result).isTrue();
     }
+
+    @Test
+    public void checkWrongAttemptTest() {
+        // given
+        Multiplication multiplication = new Multiplication(50, 60);
+        User user = new User("Daeeun");
+        MultiplicationResultAttempt attempt =
+                new MultiplicationResultAttempt(user, multiplication, 2000);
+
+        // when
+        boolean result = multiplicationServiceImpl.checkAttempt(attempt);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+//    @Test
+//    public void createRandomMultiplicationTest() {
+//        // given (목 객체가 처음에 50, 다음에 30을 반환하도록 설정)
+//        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+//
+//        // when
+//        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+//
+//        // then
+//        assertThat(multiplication.getFactorA()).isEqualTo(50);
+//        assertThat(multiplication.getFactorB()).isEqualTo(30);
+//        assertThat(multiplication.getResult()).isEqualTo(1500);
+//    }
 }
