bc483401c1680d7d3681dd22783e1f0af058b3e7
acisliver <7273322@naver.com>
2022-04-04 05:51:55 +0900

    #1 테스트 코드 작성 및 컨트롤러 생성

---
+++ src/main/java/microservices/book/socialmultiplication/web/MultiplicationController.java
@@ -0,0 +1,16 @@
+package microservices.book.socialmultiplication.web;
+
+import microservices.book.socialmultiplication.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MultiplicationController {
+
+    private final MultiplicationService multiplicationService;
+
+    @Autowired
+    public MultiplicationController(final MultiplicationService multiplicationService) {
+        this.multiplicationService = multiplicationService;
+    }
+}
---
+++ src/test/java/microservices/book/socialmultiplication/web/MultiplicationControllerTest.java
@@ -0,0 +1,59 @@
+package microservices.book.socialmultiplication.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import microservices.book.socialmultiplication.domain.Multiplication;
+import microservices.book.socialmultiplication.service.MultiplicationService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MultiplicationController.class) // MVC 레이어(컨트롤러)와 관련된 설정만 불러온다.
+public class MultiplicationControllerTest {
+
+    @MockBean   // 스프링컨테이너가 빈을 주입해주기 때문에 Mock대신 MockBean 사용
+    private MultiplicationService multiplicationService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    // 이 객체는 initFields() 메서드를 이용해 자동으로 초기화
+    private JacksonTester<Multiplication> json;
+
+    @Before
+    public void setup() {
+        JacksonTester.initFields(this, new ObjectMapper());
+    }
+
+    @Test
+    public void getRandomMultiplicationTest() throws Exception {
+        // given
+        given(multiplicationService.createRandomMultiplication())
+                .willReturn(new Multiplication(70, 20));
+
+        // when
+        MockHttpServletResponse response = mvc.perform(
+                        get("/multiplications/random")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString())
+                .isEqualTo(json.write(new Multiplication(70, 20)).getJson());
+    }
+}
