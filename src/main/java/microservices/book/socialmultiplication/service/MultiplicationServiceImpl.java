4143ccd9db0cd02a2980d372f9526f0dc117bc55
acisliver <7273322@naver.com>
2022-04-04 04:34:39 +0900

    비즈니스 로직 레이어 요구사항 - 제출한 답안의 정답 여부 확인

--- src/main/java/microservices/book/socialmultiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/microservices/book/socialmultiplication/service/MultiplicationServiceImpl.java
@@ -1,6 +1,7 @@
 package microservices.book.socialmultiplication.service;
 
 import microservices.book.socialmultiplication.domain.Multiplication;
+import microservices.book.socialmultiplication.domain.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +21,11 @@ public class MultiplicationServiceImpl implements MultiplicationService{
         int factorB = randomGeneratorService.generateRandomFactor();
         return new Multiplication(factorA, factorB);
     }
+
+    @Override
+    public boolean checkAttempt(MultiplicationResultAttempt resultAttempt) {
+        return false;
+    }
+
+
 }
