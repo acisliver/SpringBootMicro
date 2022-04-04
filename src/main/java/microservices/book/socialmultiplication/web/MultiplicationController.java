cfa5a18ac1dba2584a8e710cc0f473c89fa2f123
acisliver <7273322@naver.com>
2022-04-04 14:02:23 +0900

    #1 테스트 코드 작성 및 컨트롤러 생성1

--- src/main/java/microservices/book/socialmultiplication/web/MultiplicationController.java
+++ src/main/java/microservices/book/socialmultiplication/web/MultiplicationController.java
@@ -1,10 +1,14 @@
 package microservices.book.socialmultiplication.web;
 
+import microservices.book.socialmultiplication.domain.Multiplication;
 import microservices.book.socialmultiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/multiplications")
 public class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
@@ -12,5 +16,10 @@ public class MultiplicationController {
     @Autowired
     public MultiplicationController(final MultiplicationService multiplicationService) {
         this.multiplicationService = multiplicationService;
+    }
+
+    @GetMapping("/random")
+    Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
     }
 }
