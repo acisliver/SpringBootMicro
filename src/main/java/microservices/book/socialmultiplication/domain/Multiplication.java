4a61d44948f571ea795980c99078b68c2066cf72
acisliver <7273322@naver.com>
2022-04-08 04:20:53 +0900

    데이터 모델링

--- src/main/java/microservices/book/socialmultiplication/domain/Multiplication.java
+++ src/main/java/microservices/book/socialmultiplication/domain/Multiplication.java
@@ -5,6 +5,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * 애플리케이션에서 곱셈을 나타내는 클래스
  */
@@ -12,14 +17,20 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
-public class Multiplication {
+@Entity
+public final class Multiplication {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MULTIPLICATION_ID")
+    private Long id;
 
     // 인수
     private final int factorA;
     private final int factorB;
 
     // JSON (역)직렬화를 위한 빈 생성자
-    Multiplication() {
+    public Multiplication() {
         this(0, 0);
     }
 }
--- src/main/java/microservices/book/socialmultiplication/domain/MultiplicationResultAttempt.java
+++ src/main/java/microservices/book/socialmultiplication/domain/MultiplicationResultAttempt.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 /**
  * {@link User}가 {@link Multiplication}을 계산한 답안을 정의한 클래스
  */
@@ -12,16 +14,25 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
-public class MultiplicationResultAttempt {
+@Entity
+public final class MultiplicationResultAttempt {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
     private final User user;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "MULTIPLICATION_ID")
     private final Multiplication multiplication;
     private final int resultAttempt;
 
     private final boolean correct;
 
     // JSON (역)직렬화를 위한 빈 생성자
-    MultiplicationResultAttempt() {
+    public MultiplicationResultAttempt() {
         user = null;
         multiplication = null;
         resultAttempt = -1;
