a2d14f757f99712b6e63a09d9165bbc956fe62cc
acisliver <7273322@naver.com>
2022-04-08 05:27:48 +0900

    리포지토리 생성

---
+++ src/main/java/microservices/book/socialmultiplication/repository/MutiplicationRepository.java
@@ -0,0 +1,10 @@
+package microservices.book.socialmultiplication.repository;
+
+import microservices.book.socialmultiplication.domain.Multiplication;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * {@link Multiplication}을 저장하고 조회하기 위한 인터페이스
+ */
+public interface MutiplicationRepository extends CrudRepository<Multiplication, Long> {
+}
